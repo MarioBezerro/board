81b8aadba98cad17198027650f082a8d1b0b436e
Mario <devmajb@gmail.com>
2025-07-21 16:58:01 -0300

    Atualização do código

--- src/main/java/br/com/board/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/board/persistence/dao/BoardColumnDAO.java
@@ -1,4 +1,28 @@
 package br.com.board.persistence.dao;
 
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
 public class BoardColumnDAO {
+
+    private final Connection connection;
+
+    public BoardcolumnEntity insert(final BoardColumnEntity entity) throws SQLException {
+        var sql = "INSERT INTO BOARDS_COLUMNS (name, `order`, kind, board_id) VALUES (?, ?, ?, ?);";
+        try (var statment = connection.prepareStatement(sql)) {
+            var i = 1;
+            statment.setString(i ++, entity.getName());
+            statement.setInt(i ++, entity.getOrder());
+            statement.setString(i ++, entity.getKind().name());
+            statement.setLong(i, entity.getBoard().getId());
+            statement.executeUpdate();
+            if (statement instanceof StatementImpl impl){
+                entity.setId(impl.getLastInsertID());
+            }
+            return entity;
+        }
+    }
 }
